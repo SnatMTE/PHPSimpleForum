1ad358ac51f4859cfd4324f00c5d6b4557a3562f
SnatMTE <120643725+SnatMTE@users.noreply.github.com>
2023-02-06 17:43:25 +0000

    Updated variables.

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+//Simpleish forum written by Snat.
+//Do as you wish with it, I was bored.
+
+$host = '';
+$dbname = '';
+$username = '';
+$password = '';
+
+//Below is general config details. 
+
+$site_name = "";
+$site_email = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error connecting to database: " . $e->getMessage());
+}
+?>
